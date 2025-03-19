90ca15d44de935454971e55069f8bfdef3ba14be
KairuMaina <george.kairu@student.moringaschool.com>
2025-03-19 10:51:34 +0300

    Implemented list comprehension functions with passing tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+def return_evens(numbers):
+    """Returns a list of even numbers from the input list using list comprehension."""
+    return [num for num in numbers if num % 2 == 0]
 
-def return_evens(num_list):
-    pass
-
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentences):
+    """Adds an exclamation mark at the end of each sentence in the list using list comprehension."""
+    return [sentence + "!" for sentence in sentences]
